f98a5d45c9d1320dd3f2ff8ab3be4a72e3871119
Adam Blevins <blevinspercussion@gmail.com>
2023-01-17 14:30:23 -0600

    Added js logic for the nav menu and wrote code to popular 'services' item

---
+++ script.js
@@ -0,0 +1,27 @@
+// Get DOM Elements 
+const menuServices = document.getElementById("menu-services");
+const menuBio = document.getElementById("menu-bio");
+const menuContact = document.getElementById("menu-contact");
+
+// Function to create sub menus
+function createSubMenu(parentElement, ulID, ...elements) {
+    const newUL = document.createElement("ul");
+    newUL.id = ulID;
+    newUL.className = "sub-menu";
+    for (let i=0; i<elements.length; i++) {
+        let newLI = document.createElement("li");
+        newLI.className = "li-" + elements[i];
+        newLI.textContent = elements[i];
+        newUL.appendChild(newLI);
+        console.log(newLI);
+    }
+    parentElement.appendChild(newUL);
+    console.log(newUL);
+    console.log(parentElement);
+}
+
+// Creat sub menus
+menuServices.addEventListener("click", () => {
+    document.getElementById("services-sub-menu") === null ? createSubMenu(menuServices, "services-sub-menu", "lessons", "live performance", "studio") : document.getElementById("services-sub-menu").remove()
+})
+        
