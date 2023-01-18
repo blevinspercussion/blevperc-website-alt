ecab9209ae385baacfc1b0bf5d5ed86b15d2261e
Adam Blevins <blevinspercussion@gmail.com>
2023-01-18 11:43:09 -0600

    Added js logic to make hamburger menu work

--- script.js
+++ script.js
@@ -2,9 +2,14 @@
 const menuServices = document.getElementById("menu-services");
 const menuBio = document.getElementById("menu-bio");
 const menuContact = document.getElementById("menu-contact");
+const navMenu = document.getElementById("nav-menu");
+const hamburger = document.getElementById("hamburger");
+
+console.log(navMenu.style.visibility);
 
 // Function to create sub menus
 function createSubMenu(parentElement, ulID, ...elements) {
+    console.log("mouseover");
     const newUL = document.createElement("ul");
     newUL.id = ulID;
     newUL.className = "sub-menu";
@@ -21,4 +26,23 @@ function createSubMenu(parentElement, ulID, ...elements) {
 menuServices.addEventListener("click", () => {
     document.getElementById("services-sub-menu") === null ? createSubMenu(menuServices, "services-sub-menu", "lessons", "live performance", "studio") : document.getElementById("services-sub-menu").remove()
 })
-        
+
+// hamburger.addEventListener("click", () => {
+//     console.log(navMenu.style.visibility);
+//     if (navMenu.style.visibility === "hidden"){
+//         navMenu.style.visibility = "visible";
+//     } else if (navMenu.style.visibility === "visible") {
+//         navMenu.style.visibility = "hidden";
+//     }
+
+//     navMenu.style.visibility === "hidden" ? navMenu.style.visibility = "visible" : navMenu.style.visibility = "hidden"
+// })
+
+hamburger.addEventListener("click", () => {
+    console.log('hamburger clicked');
+    if (navMenu.style.visibility != 'visible') {
+        navMenu.style.visibility = 'visible';
+    } else {
+        navMenu.style.visibility = 'hidden';
+    }
+})
