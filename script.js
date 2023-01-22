5ac72f54e121bb9adab2856912f7e9555f0b2cbc
Adam Blevins <blevinspercussion@gmail.com>
2023-01-22 07:11:12 -0600

    Added text content to index.html and made header a custom webcomponent

--- script.js
+++ script.js
@@ -1,3 +1,34 @@
+
+
+// Define custom elements/web components
+class Header extends HTMLElement {
+    constructor() {
+        super();
+    }
+    connectedCallback() {
+        this.innerHTML = `
+            <header>
+                <img class="logo" src="./media/pics/logo.png">
+                <div>
+                    <img id="hamburger" src="./media/icons/icons8-menu-rounded-30.png">
+                    <nav class="nav-hidden">
+                        <ul class="submenu">
+                            <li>Lessons</li>
+                            <li>Live</li>
+                            <li>Studio</li>
+                            <li>Media</li>
+                            <li>About</li>
+                            <li class="attention-text">Hire Me!</li>
+                        </ul>
+                    </nav>
+                </div>
+            </header>
+    `
+    }
+}
+
+customElements.define('my-header', Header);
+
 const body = document.querySelector('body');
 const hamburger = document.getElementById('hamburger');
 const navMenu = document.querySelector('nav');
